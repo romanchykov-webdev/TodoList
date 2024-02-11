81a2b52ce77c73c6cdcea7a2338edad423c2489a
romanchykov-webdev <romanchykov.webdev@gmail.com>
2024-02-11 18:33:15 +0100

    v16

--- serverMongo/index.js
+++ serverMongo/index.js
@@ -1,5 +1,5 @@
 const path = require('path')
-process.env['NODE_CONFIG_DIR'] = path.join(path.resolve("./"),"config/")
+process.env['NODE_CONFIG_DIR'] = path.join(path.resolve("./"),".env")
 const chalk = require('chalk');
 
 const errorMsg = chalk.bgKeyword('red').whiteBright
