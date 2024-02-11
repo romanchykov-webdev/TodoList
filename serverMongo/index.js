0250a3236aadc719b66fee87e07d6098795938ee
romanchykov-webdev <romanchykov.webdev@gmail.com>
2024-02-11 18:57:11 +0100

    v19

--- serverMongo/index.js
+++ serverMongo/index.js
@@ -8,12 +8,12 @@ const dotenv = require('dotenv');
 const app = express();
 
 // Установка переменной окружения для пути к файлу конфигурации
-dotenv.config({ path: './.env' });
+dotenv.config({ path: '.env' });
 
 // Порт и URL для MongoDB должны браться из переменных окружения или файла конфигурации
-const PORT = process.env.PORT || config.get('serverPort');
-const MONGO_URL = process.env.MONGO_URL || config.get('dbUrl');
-const PRIVATE_KEY = process.env.PRIVATE_KEY || config.get('secretKey');
+const PORT = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 // Остальные настройки сервера...
 
